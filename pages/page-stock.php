e1a7b8db00d01d4171acf975d53051702832c29e
ss-ameer <sibuma.syedameer@gmail.com>
2024-07-29 17:23:36 +0800

    stock init

---
+++ pages/page-stock.php
@@ -0,0 +1,26 @@
+<!-- page-stock.php -->
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stocks</title>
+
+    <!--styling -->
+    <link rel="stylesheet" href="<?php echo ROOT . 'imports/bootstrap/css/bootstrap.css' ?>">
+    <link rel="stylesheet" href="<?php echo ROOT . 'imports/bootstrap/bootstrap-icons-1.11.3/font/bootstrap-icons.css' ?>">
+    <link rel="stylesheet" href="<?php echo ROOT . 'styles/style.css' ?>">
+    
+    <!-- javascript -->
+    <script src="<?php echo ROOT . 'imports/bootstrap/js/bootstrap.bundle.js'?>"></script>
+    <script src="<?php echo ROOT . 'imports/jquery/jquery-3.7.1.min.js' ?>"></script>
+    <?php include_once(PATH . 'configs/config-script.php'); ?>
+
+</head>
+<body>
+    
+</body>
+</html>
