4e9d738bc596b79d29599ecd7a01c517ee2147bb
ss-ameer <sibuma.syedameer@gmail.com>
2024-08-02 23:26:59 +0800

    master: c( stock section )

--- pages/page-master.php
+++ pages/page-master.php
@@ -25,42 +25,134 @@
 
 </header>
 <main>
-    <div class="container-fluid vh-100">
-    <div class="row h-100">
-    <!-- sidebar -->
-    <?php include_once(PATH . 'components/comp-nav-side.php'); ?>
-    <!-- master list -->
-    <div class="col mh-100 overflow-auto p-3 border">
-        <p class="lead">Master</p>
-        <ul class="list-group">
-        <li class="list-group-item">
-            <div data-bs-toggle="collapse" data-bs-target="#master-register">
-                <a class="" href="#">Create an Account</a>
+
+<div class="container-fluid vh-100">
+<div class="row h-100">
+<!-- sidebar -->
+<?php include_once(PATH . 'components/comp-nav-side.php'); ?>
+<!-- master list -->
+<div class="col mh-100 overflow-auto p-3 border">
+    <p class="lead">Master</p>
+    <ul class="list-group">
+    <li class="list-group-item">
+        <div data-bs-toggle="collapse" data-bs-target="#master-register">
+            <a class="" href="#">Create an Account</a>
+        </div>
+
+        <div class="collapse container" id="master-register">
+            <hr>
+            <!-- register form -->
+            <?php include_once('../components/comp-form-register.php') ?>
+        </div>        
+    </li>
+
+    <li class="list-group-item">
+        <div data-bs-toggle="collapse" data-bs-target="#master-view">
+            <!-- account view -->
+            <a class="" href="#">View Accounts</a>
+        </div>
+
+        <div class="collapse container" id="master-view">
+            <hr>
+            <!-- officers table -->
+            <?php include_once('../components/comp-table-officers.php') ?>
+        </div>
+    </li>
+
+    <li class="list-group-item">
+        <div data-bs-toggle="collapse" data-bs-target="#master-stocks">
+            <!-- manage stocks -->
+            <a href="#" class="">Manage Stocks</a>
+        </div>
+        
+        <div class="collapse container" id="master-stocks">
+            <hr>
+            <div class="d-flex">
+                <div class="w-50 border rounded shadow-sm p-3">
+                    <table class="table table-hover table-borderless">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Category</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Stock</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                        <?php getStocks(); ?>
+                        <?php foreach ($_SESSION['items'] as $item) : ?>
+
+                        <tr class="item" data-id_item="<?= $item['item_id'] ?>">
+                            <td><?= $item['item_id'] ?></td>
+                            <td><?= $item['item_name'] ?></td>
+                            <td><?= $item['category'] ?></td>
+                            <td><?= $item['price'] ?></td>
+                            <td><?= $item['quantity_in_stock'] ?></td>
+                        </tr>
+
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- --- -->
+                <div class="ms-3 w-50">
+                    <form class="border rounded p-3 mb-3 shadow-sm" id="item-add">
+                    <label class="form-label">Add Item</label>
+                    <div class="form-floating mb-1">
+                        <input name="name" type="text" id="item-add_name" class="form-control" placeholder="Name">
+                        <label for="item-add_name">Name</label>
+                    </div>
+                    <div class="form-floating mb-1">
+                        <select name="category" id="item-add_category" class="form-select">
+                            <option selected>Choose</option>
+                            <option value="test">Test</option>
+                        </select>
+                        <label for="item-add_category">Category</label>
+                    </div>
+                    <div class="form-floating mb-1">
+                        <select name="uom" id="item-add_oum" placeholder="oum" class="form-select">
+                            <option selected>Choose</option>
+                            <option value="test">Test</option>
+                        </select>
+                        <label for="item-add_oum">Unit</label>
+                    </div>
+                    <div class="form-floating mb-1">
+                        <input name="price" type="number" id="item-add_name" class="form-control" placeholder="Price">
+                        <label for="item-add_name">Price</label>
+                    </div>
+                    <div class="form-floating mb-1">
+                        <textarea name="description" id="item-add_description" class="form-control" placeholder="Description"></textarea>
+                        <label for="item-add_description">Description</label>
+                    </div>
+                    <button class="btn btn-success ms-auto" type="submit">Add</button>
+                    </form>
+                    <!-- --- -->
+                    <form class="border rounded p-3 shadow-sm">
+                    <label class="form-label">Add Stocks</label>
+                    <div class="input-group mb-1">
+                        <span class="input-group-text">ID</span>
+                        <input type="number" id="stock-add_id" class="form-control">
+                    </div>
+                    <div class="input-group mb-1">
+                        <span class="input-group-text">Name</span>
+                        <input type="text" id="stock-add_name" class="form-control" value="placeholder" disabled>
+                    </div>
+                    <div class="input-group mb-1">
+                        <span class="input-group-text">Quantity</span>
+                        <input type="number" id="stock-add_quantity" class="form-control" value="placeholder">
+                    </div>
+                    <button class="btn btn-success ms-auto" type="submit">Add</button>
+                    </form>
+                </div>
             </div>
+        </div>
+    </li>
 
-            <div class="collapse container" id="master-register">
-                <hr>
-                <!-- register form -->
-                <?php include_once('../components/comp-form-register.php') ?>
-            </div>        
-        </li>
-
-        <li class="list-group-item">
-            <div data-bs-toggle="collapse" data-bs-target="#master-view">
-                <a class="" href="#">View Accounts</a>
-            </div>
-
-            <div class="collapse container" id="master-view">
-                <hr>
-                <!-- officers table -->
-                <?php include_once('../components/comp-table-officers.php') ?>
-            </div>
-        </li>
-
-        </ul>
-    </div>
-    </div>
-    </div>
+    </ul>
+</div>
+</div>
+</div>
 
 </main>
 <footer>
