5fd429d534f1397afd881776d850cb38f8f3c0b7
ss-ameer <sibuma.syedameer@gmail.com>
2024-10-09 15:52:32 +0800

    header: initialization

---
+++ components/comp-header.php
@@ -0,0 +1,12 @@
+<!-- comp-header.php -->
+<div class="row border shadow" style="height: 10%; max-height: 10%;">
+    <div class="col">
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <div class="navbar-brand">
+                    Eslaya
+                </div>
+            </div>
+        </nav>
+    </div>
+</div>
