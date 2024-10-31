5d8d7ba3179beb0378a696052cfdafabb1207b5e
ss-ameer <sibuma.syedameer@gmail.com>
2024-10-31 13:09:23 +0800

    master:  edit modal edit

--- components/comp-modal-edit.php
+++ components/comp-modal-edit.php
@@ -1,19 +1,21 @@
 <!-- Modal HTML -->
 <div id="edit-modal" class="modal fade" tabindex="-1" aria-labelledby="edit-modal-label" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="edit-modal-label">Edit Data</h5>
-                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">&times;</button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form id="edit-form">
-                    <!-- Dynamic form fields will be appended here by jQuery -->
-                </form>
+                <div class="container">
+                    <form id="edit-form">
+                        <!-- Dynamic form fields will be appended here by jQuery -->
+                    </form>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary" id="saveChangesBtn">Save changes</button>
+                <button type="submit" class="btn btn-primary" id="edit-form-submit">Save changes</button>
             </div>
         </div>
     </div>
