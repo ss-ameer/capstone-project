42270dc8dad5cf470f5a812641eb1819b3f3e108
ss-ameer <sibuma.syedameer@gmail.com>
2024-11-10 21:32:38 +0800

    sir drew: orders from drop to tab

--- pages/page-orders.php
+++ pages/page-orders.php
@@ -36,103 +36,127 @@
                 <?php include_once(PATH . 'components/comp-nav-side.php'); ?>
                 <!-- main -->
                 <div class="col mh-100 overflow-auto">
-                    <div class="container">
-                        <div class="row mt-3 g-3">
-                            <div class="col-12">
+                    <div class="container py-3">
+
+                            <!-- <div class="col-12">
                                 <div class="c-section-toggler" data-bs-toggle="collapse" data-bs-target=".order-create">
                                     <a class="fs-5" href="#">Create Order</a>
                                 </div>
+                            </div> -->
+
+                        <ul class="nav nav-tabs" id="tab-orders" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link active" id="orders-create-tab" data-bs-toggle="tab" data-bs-target="#orders-create" type="button" role="tab">Create</button>
+                            </li>
+
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link" id="orders-views-tab" data-bs-toggle="tab" data-bs-target="#orders-views" type="button" role="tab">Views</button>
+                            </li>
+                        </ul>
+
+                        <div class="tab-content" id="tab-content-orders">
+
+                            <div class="tab-pane fade show active" id="orders-create" role="tabpanel">
+                                <div class="container">
+                                    <div class="row mt-1 g-3">
+
+                                        <div class="col-6 order-create">
+                                            <div class="c-section">
+                                                <div class="c-section-header">
+                                                    <div class="c-title-container">
+                                                        <span class="lead">Client Info</span>
+                                                    </div>
+                                                </div>
+
+                                                <div class="p-3 order-form-container">
+                                                    <?php include_once('../components/comp-form-order.php'); ?>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-6 order-create">
+                                            <div class="container p-0 border rounded">
+                                                <div class="c-section-header">
+                                                    <div class="c-title-container">
+                                                        <span class="lead">Item Search</span>
+                                                    </div>
+                                                </div>
+
+                                                <div class="p-3">
+                                                    <div class="input-group">
+                                                        <input type="search" name="item search" autocomplete="off" class="form-control" id="item-search">
+                                                        <span class="input-group-text"><i class="bi bi-search"> Search</i></span>
+                                                    </div>
+                                                    <ul id="item-suggestions" class="list-group mt-2 w-100 mh-100" style="display: none;"></ul>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12 order-create">
+                                            <div class="c-table-container">
+                                                <table class="table align-middle" id="order-items-table">
+                                                    <thead class="table-primary">
+                                                        <tr class="text-center">
+                                                            <th scope="col">ID</th>
+                                                            <th scope="col">Name</th>
+                                                            <th scope="col">Unit</th></th>
+                                                            <th scope="col">Quantity</th>
+                                                            <th scope="col">Price(m³)</th>
+                                                            <th scope="col">TPrice</th>
+                                                            <th scope="col">Action</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12 order-create">
+                                            <div class="p-3 border rounded d-flex justify-content-between">
+                                                <div>
+                                                    <button class="btn btn-outline fw-light" id="order-items-total_qty">Total Quantity: <span>00</span></button>
+                                                    <button class="btn btn-outline fw-light" id="order-items-total_price">Total Price: <span>00.00</span></button>
+                                                </div>
+                                                <div>
+                                                    <button type="button" class="btn btn-outline-primary" id="preview-order-btn" data-bs-toggle="modal" data-bs-target="#orderPreviewModal">Order Preview</button>
+                                                    <button type="submit" form="order-form" class="btn btn-outline-success">Create Order</button>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
                             </div>
+
+                            <div class="tab-pane fade" id="orders-views" role="tabpanel">
+                                <div class="container">
+                                    <div class="row mt-1 g-3">
+                                        <!-- <div class="col-12">
+                                            <div class="c-section-toggler" data-bs-toggle="collapse" data-bs-target=".order-view">
+                                                <a class="fs-5" href="#">Order Tables</a>
+                                            </div>
+                                        </div> -->
+
+                                        <div class="col-12 order-view">
+                                            <div class="c-section bg-light">
+                                                <div class="c-section-header_table bg-light">
+                                                    <div class="c-title-container">
+                                                        <span class="lead">Orders</span>
+                                                    </div>
+                                                </div>
+                                                
+                                                <div class="c-table-container orders-table-container">
+                                                    <?php include PATH . 'components/comp-table-orders.php'; ?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                         
-                            <div class="col-6 collapse order-create">
-                                <div class="c-section">
-                                    <div class="c-section-header">
-                                        <div class="c-title-container">
-                                            <span class="lead">Client Info</span>
-                                        </div>
-                                    </div>
-
-                                    <div class="p-3 order-form-container">
-                                        <?php include_once('../components/comp-form-order.php'); ?>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-6 collapse order-create">
-                                <div class="container p-0 border rounded">
-                                    <div class="c-section-header">
-                                        <div class="c-title-container">
-                                            <span class="lead">Item Search</span>
-                                        </div>
-                                    </div>
-
-                                    <div class="p-3">
-                                        <div class="input-group">
-                                            <input type="search" name="item search" autocomplete="off" class="form-control" id="item-search">
-                                            <span class="input-group-text"><i class="bi bi-search"> Search</i></span>
-                                        </div>
-                                        <ul id="item-suggestions" class="list-group mt-2 w-100 mh-100" style="display: none;"></ul>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-12 collapse order-create">
-                                <div class="c-table-container">
-                                    <table class="table align-middle" id="order-items-table">
-                                        <thead class="table-primary">
-                                            <tr class="text-center">
-                                                <th scope="col">ID</th>
-                                                <th scope="col">Name</th>
-                                                <th scope="col">Unit</th></th>
-                                                <th scope="col">Quantity</th>
-                                                <th scope="col">Price(m³)</th>
-                                                <th scope="col">TPrice</th>
-                                                <th scope="col">Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-
-                            <div class="col-12 collapse order-create">
-                                <div class="p-3 border rounded d-flex justify-content-between">
-                                    <div>
-                                        <button class="btn btn-outline fw-light" id="order-items-total_qty">Total Quantity: <span>00</span></button>
-                                        <button class="btn btn-outline fw-light" id="order-items-total_price">Total Price: <span>00.00</span></button>
-                                    </div>
-                                    <div>
-                                        <button type="button" class="btn btn-outline-primary" id="preview-order-btn" data-bs-toggle="modal" data-bs-target="#orderPreviewModal">Order Preview</button>
-                                        <button type="submit" form="order-form" class="btn btn-outline-success">Create Order</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mt-1 g-3">
-                            <div class="col-12">
-                                <div class="c-section-toggler" data-bs-toggle="collapse" data-bs-target=".order-view">
-                                    <a class="fs-5" href="#">Order Tables</a>
-                                </div>
-
-                            </div>
-
-                            <div class="col-12 collapse order-view">
-                                <div class="c-section bg-light">
-                                    <div class="c-section-header_table bg-light">
-                                        <div class="c-title-container">
-                                            <span class="lead">Orders</span>
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="c-table-container orders-table-container">
-                                        <?php include PATH . 'components/comp-table-orders.php'; ?>
-                                    </div>
-                                </div>
-                                
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
