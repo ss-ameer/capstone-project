d04c0aba994e63a06fe2b54617ff4d85d1082b36
ss-ameer <sibuma.syedameer@gmail.com>
2024-11-11 22:38:22 +0800

    dashboard: reconfiguration

--- pages/page-dashboard.php
+++ pages/page-dashboard.php
@@ -1,12 +1,16 @@
 <!-- page-dashboard.php -->
 <?php 
 
-    include_once('../configs/config-function.php')
+    include_once('../configs/config-function.php');
+    $officer_id = getCurrentOfficer('id');
+    $officer_info = dbGetTableData('dispatch_officers', '*', '', "id = $officer_id")[0];
+
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
@@ -19,40 +23,46 @@
     <!-- javascript -->
     <script src="<?php echo ROOT . 'imports/bootstrap/js/bootstrap.bundle.js'?>"></script>
     <script src="<?php echo ROOT . 'imports/jquery/jquery-3.7.1.min.js' ?>"></script>
-    <?php include_once(PATH . 'configs/config-script.php'); ?>
+    <?php include PATH . 'configs/config-script.php'; ?>
 
 </head>
 <body>
-<header> 
-    
-</header>
-<main>
-    <div class="container-fluid vh-100">
-        <div class="row h-100">
-            <?php include_once(PATH . 'components/comp-nav-side.php'); ?>
-        <div class="col mh-100 overflow-auto p-3 border">
-            <p class="lead">Dashboard</p>
-            <div class="container border rounded p-3">
-                #<?= $_SESSION['user_info']['id'] ?>
-                <?= $_SESSION['user_info']['name'] ?>
-            </div>
-            <div class="container rounded border p-3 px-0 d-flex">
-                <div class="border rounded p-3">
-                    Dispatch:
-                </div>
-                <div class="border rounded p-3">
-                    Trucks:
-                </div>
-                <div class="border rounded p-3">
-                    Drivers:
-                </div>
-                <div class="border rounded p-3">
-                    Deliveries:
+
+    <main>
+        <div class="container-fluid vh-100 scrollbar-hidden">
+            <!-- header -->
+            <?php include_once PATH . 'components/comp-header.php'; ?>
+            <div class="row c-main-content">
+                <!-- sidebar -->
+                <?php include PATH . 'components/comp-nav-side.php'; ?>
+                <!-- main -->
+                <div class="col mh-100 overflow-auto border">
+                    <div class="container my-3">
+                        <div class="row g-3">
+
+                            <div class="col-12">
+                                <div class="container">
+
+                                    <div class="user-info-card my-3">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h5 class="card-title"><?php echo $officer_info['name']; ?></h5>
+                                                <p class="card-text">
+                                                    <strong>Role:</strong> <?php echo $officer_info['role']; ?><br>
+                                                    <!-- <strong>Last Login:</strong> <?php //echo date("M d, Y - h:i A", strtotime($user_info['last_login'])); ?><br> -->
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    </div>
-</main>
+    </main>
 </body>
 </html>
